a413cebbdce40e7f61d6128033a7588f4f52d84a
WongWingHo-2526 <250131330@stu.vtc.edu.hk>
2026-04-15 21:09:00 +0800

    Update start.sh

--- pegasus_computer_store/start.sh
+++ pegasus_computer_store/start.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-echo "Installing dependencies..."
-pip install -r requirements.txt
-echo "Dependencies installed. Starting Flask..."
-exec flask --debug run --host=0.0.0.0 --port=5000
+
